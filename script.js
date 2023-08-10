cefd0c8e6e9c1b98108918557084a72082c06d63
drko2004 <141055092+drko2004@users.noreply.github.com>
2023-08-11 01:02:07 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ if(isNaN(score) || score < 0){
 	alert("숫자 입력...");
 } else if(score >= 90){
 	alert("A");
-} else if(score >= 80 && score > 90){
+} else if(score >= 80 && score < 90){
 	alert("B");
-}	else if(score >= 70 && score > 80){
+}	else if(score >= 70 && score < 80){
 	alert("C");
 } else {
 	alert("D");
